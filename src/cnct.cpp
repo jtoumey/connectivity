1085f8c974ba84d8837df201be5e9d67320a78d3
Julian Toumey <jtoumey@gmail.com>
2019-06-21 08:45:31 -0400

    wip: The algorithm is ignoring boundary edges and so far I am double-counting interior edges with my attempts to fix.

--- src/cnct.cpp
+++ src/cnct.cpp
@@ -42,7 +42,7 @@ public:
     void calculateCellCenters();
     void calculateConnectivity();
     void generateEdgeList();
-    void findFaceNeighbors(int, int, int);
+    int findFaceNeighbors(int, int, int);
 
 };
 
@@ -163,39 +163,69 @@ void Geometry::calculateConnectivity()
     }
 }
 
-void Geometry::findFaceNeighbors(int current_cell_index, int edge_start_vert, int edge_end_vert)
+int Geometry::findFaceNeighbors(int current_cell_index, int edge_start_vert, int edge_end_vert)
 {
-       for (int jj = 0; jj < 4; ++jj)
+    int edge_validity;
+    int neighbor_count = 0;
+    // Loop over the four (at maximum) cells connected to the edge start vertex
+    for (int jj = 0; jj < 4; ++jj)
+    {
+        int neighbor_cell_index = vertices[edge_start_vert].neighbors[jj];
+
+        // Some vertices don't have neighbors in particular quadrants
+        if (neighbor_cell_index == -1)
         {
-            int neighbor_cell_index = vertices[edge_start_vert].neighbors[jj];
-
-            // some vertices don't have neighbors in particular quadrants
-            if (neighbor_cell_index == -1)
+            continue;
+        }
+        else
+        {
+            if (neighbor_cell_index != current_cell_index)
             {
-                continue;
+                neighbor_count++;
+                
             }
-            else
-            {
-                // Loop over associated vertices of cells that contain sw_vertex_index
-                for (int nn = 0; nn < 4; ++nn)
-                {
-                    if (cell_centers[neighbor_cell_index].neighbors[nn] == edge_end_vert && neighbor_cell_index != current_cell_index)
-                    {
-                        std::cout << "Found an e-w edge with a neighbor: verts ( " << edge_start_vert << ", " << edge_end_vert << ")" << std::endl;
-                        std::cout << "  Neighbor:" << current_cell_index << "(parent cell), with " << neighbor_cell_index << std::endl;
+            // // Loop over vertices of this shared cell 
+            // for (int nn = 0; nn < 4; ++nn)
+            // {
 
-                        if (current_cell_index < neighbor_cell_index)
-                        {
-                            std::cout << "Found a unique edge:" << std::endl;
-                        }
-                    }
-                    else
-                    {
-                        continue;
-                    }
-                }
-            }
-        }   
+            //     if (cell_centers[neighbor_cell_index].neighbors[nn] == edge_end_vert && neighbor_cell_index > current_cell_index)
+            //     {
+            //         std::cout << "Edge with vertices (" << edge_start_vert << ", " << edge_end_vert << ") is not valid." << std::endl;
+
+            //         edge_validity = 0;
+            //     }
+            //     else
+            //     {
+            //         edge_validity = 1;
+            //     }
+
+                //     neighbor_count++;
+
+                //     if (current_cell_index < neighbor_cell_index)
+                //     {
+                //         std::cout << "Edge with vertices (" << edge_start_vert << ", " << edge_end_vert << ") is unique." << std::endl;
+                //     }
+                //     else if (current_cell_index > neighbor_cell_index)
+                //     {
+                //         std::cout << "Edge with vertices (" << edge_start_vert << ", " << edge_end_vert << ") was already stored." << std::endl;
+                //     }
+                //     else if (current_cell_index == neighbor_cell_index)
+                //     {
+                //         std::cout << "Edge with vertices (" << edge_start_vert << ", " << edge_end_vert << ") is unique and on a boundary." << std::endl;
+                //     }
+                // }
+                // else if (cell_centers[neighbor_cell_index].neighbors[nn] == edge_end_vert && neighbor_cell_index == current_cell_index )
+                // {
+                //     if (current_cell_index )
+                //     {
+                //         std::cout << "Edge with vertices (" << edge_start_vert << ", " << edge_end_vert << ") is unique and on a boundary." << std::endl;
+                //     }
+                // }             
+            // }
+        }
+    }
+    return(edge_validity);
+    // std::cout << "Edge with vertices (" << edge_start_vert << ", " << edge_end_vert << ") has " << neighbor_count << " neighbors." << std::endl;
 }
 
 void Geometry::generateEdgeList()
@@ -203,16 +233,33 @@ void Geometry::generateEdgeList()
     // Loop over all cells
     for (int ii = 0; ii < geometry_params.np; ++ii)
     {
-        // For a South face of the cell, loop over the 
+        // For a South face of the cell, collect the SW and SE vertices as the start and end
         int sw_vertex_index = cell_centers[ii].neighbors[0];
         int se_vertex_index = cell_centers[ii].neighbors[1];
 
-        findFaceNeighbors(ii, sw_vertex_index, se_vertex_index);
+        //create the edge here
+        // return a boolean for if edge is unique
+        int s_edge_validity = findFaceNeighbors(ii, sw_vertex_index, se_vertex_index);
+
+        if (s_edge_validity == 1)
+        {
+            std::cout << "Edge with vertices (" << sw_vertex_index << ", " << se_vertex_index << ") is unique." << std::endl;
+            // if yes, push onto edge list
+        }
+
+
+        int ne_vertex_index = cell_centers[ii].neighbors[2];
+
+        findFaceNeighbors(ii, se_vertex_index, ne_vertex_index);
+
+        int nw_vertex_index = cell_centers[ii].neighbors[3];
+
+        findFaceNeighbors(ii, ne_vertex_index, nw_vertex_index);
+
+        findFaceNeighbors(ii, nw_vertex_index, sw_vertex_index);
+
     }
 }
-
-    //     // Loop over cells that use the sw_vertex_index 
- 
 
     //     int ne_vertex_index = cell_centers[ii].neighbors[2];
 
