6fe226f64e246134639653a4e7bc5a71aaf45cef
Julian Toumey <jtoumey@gmail.com>
2019-06-21 21:51:17 -0400

    wip: Fixed bug in which I used the  command erroneously, causing loop incrementation when I didn't want it. Now, the code adds boundary edges for South and East boundaries.

--- src/cnct.cpp
+++ src/cnct.cpp
@@ -258,118 +258,128 @@ void Geometry::generateEdgeList()
         // return a boolean for if edge is unique
         int neighbor_count = findFaceNeighbors(ii, sw_vertex_index, se_vertex_index);
 
-        if (neighbor_count > 0)
+        if (neighbor_count == 0)
         {
-            std::cout << "Edge with vertices (" << sw_vertex_index << ", " << se_vertex_index << ") has two neighbors." << std::endl;
-            // if yes, loop again and test for uniqueness
-            int unique_nbr_flag = getFaceNeighbors(ii, sw_vertex_index, se_vertex_index);
-
-            if (unique_nbr_flag == 1)
-            {
-                std::cout << "  This edge is also unique." << std::endl;
-
-                edge_list.push_back(edge_c);
-            }
-            else
-            {
-                continue;
-            }
+            edge_list.push_back(edge_c);
         }
         else
         {
-            std::cout << "Edge with vertices (" << sw_vertex_index << ", " << se_vertex_index << ") is on a boundary and hence unique." << std::endl;
-
-            edge_list.push_back(edge_c);   
+            
         }
-
-
+        
+        // For an East face of the cell, collect the SE and NE vertices as the start and end
         int ne_vertex_index = cell_centers[ii].neighbors[2];
 
         //create the edge here
         edge_c.start_vertex = se_vertex_index;
         edge_c.end_vertex = ne_vertex_index;
 
+        // return a boolean for if edge is unique
         neighbor_count = findFaceNeighbors(ii, se_vertex_index, ne_vertex_index);
-        if (neighbor_count > 0)
-        {
-            std::cout << "Edge with vertices (" << se_vertex_index << ", " << ne_vertex_index << ") has two neighbors." << std::endl;
-            // if yes, loop again and test for uniqueness
-            int unique_nbr_flag = getFaceNeighbors(ii, se_vertex_index, ne_vertex_index);
 
-            if (unique_nbr_flag == 1)
-            {
-                std::cout << "  This edge is also unique." << std::endl;
-
-                edge_list.push_back(edge_c);
-            }
-            else
-            {
-                continue;
-            }
-        }
-        else
+        if (neighbor_count == 0)
         {
-            std::cout << "Edge with vertices (" << se_vertex_index << ", " << ne_vertex_index << ") is on a boundary and hence unique." << std::endl;
             edge_list.push_back(edge_c);
         }
-
-        int nw_vertex_index = cell_centers[ii].neighbors[3];
-
-        edge_c.start_vertex = ne_vertex_index;
-        edge_c.end_vertex = nw_vertex_index;
-
-        neighbor_count = findFaceNeighbors(ii, ne_vertex_index, nw_vertex_index);
-        if (neighbor_count > 0)
-        {
-            std::cout << "Edge with vertices (" << ne_vertex_index << ", " << nw_vertex_index << ") has two neighbors." << std::endl;
-            // if yes, loop again and test for uniqueness
-            int unique_nbr_flag = getFaceNeighbors(ii, ne_vertex_index, nw_vertex_index);
-
-            if (unique_nbr_flag == 1)
-            {
-                std::cout << "  This edge is also unique." << std::endl;
-
-                edge_list.push_back(edge_c);
-            }
-            else
-            {
-                continue;
-            }
-        }
         else
         {
-            std::cout << "Edge with vertices (" << ne_vertex_index << ", " << nw_vertex_index << ") is on a boundary and hence unique." << std::endl;
-            edge_list.push_back(edge_c); 
+       
         }
+        // else
+        // {
+        //     std::cout << "Edge with vertices (" << sw_vertex_index << ", " << se_vertex_index << ") is on a boundary and hence unique." << std::endl;
 
-        edge_c.start_vertex = nw_vertex_index;
-        edge_c.end_vertex = sw_vertex_index;
+        //     edge_list.push_back(edge_c);   
+        // }
 
 
-        neighbor_count = findFaceNeighbors(ii, nw_vertex_index, sw_vertex_index);
-        if (neighbor_count > 0)
-        {
-            std::cout << "Edge with vertices (" << nw_vertex_index << ", " << sw_vertex_index << ") has two neighbors." << std::endl;
-            // if yes, loop again and test for uniqueness
-            int unique_nbr_flag = getFaceNeighbors(ii, nw_vertex_index, sw_vertex_index);
+        // int ne_vertex_index = cell_centers[ii].neighbors[2];
 
-            if (unique_nbr_flag == 1)
-            {
-                std::cout << "  This edge is also unique." << std::endl;
+        // //create the edge here
+        // edge_c.start_vertex = se_vertex_index;
+        // edge_c.end_vertex = ne_vertex_index;
 
-                edge_list.push_back(edge_c);
-            }
-            else
-            {
-                continue;
-            }
-        }
-        else
-        {
-            std::cout << "Edge with vertices (" << nw_vertex_index << ", " << sw_vertex_index << ") is on a boundary." << std::endl;
-            edge_list.push_back(edge_c);
+        // neighbor_count = findFaceNeighbors(ii, se_vertex_index, ne_vertex_index);
+        // if (neighbor_count > 0)
+        // {
+        //     std::cout << "Edge with vertices (" << se_vertex_index << ", " << ne_vertex_index << ") has two neighbors." << std::endl;
+        //     // if yes, loop again and test for uniqueness
+        //     int unique_nbr_flag = getFaceNeighbors(ii, se_vertex_index, ne_vertex_index);
 
-        }
+        //     if (unique_nbr_flag == 1)
+        //     {
+        //         std::cout << "  This edge is also unique." << std::endl;
+
+        //         edge_list.push_back(edge_c);
+        //     }
+        //     else
+        //     {
+        //         continue;
+        //     }
+        // }
+        // else
+        // {
+        //     std::cout << "Edge with vertices (" << se_vertex_index << ", " << ne_vertex_index << ") is on a boundary and hence unique." << std::endl;
+        //     edge_list.push_back(edge_c);
+        // }
+
+        // int nw_vertex_index = cell_centers[ii].neighbors[3];
+
+        // edge_c.start_vertex = ne_vertex_index;
+        // edge_c.end_vertex = nw_vertex_index;
+
+        // neighbor_count = findFaceNeighbors(ii, ne_vertex_index, nw_vertex_index);
+        // if (neighbor_count > 0)
+        // {
+        //     std::cout << "Edge with vertices (" << ne_vertex_index << ", " << nw_vertex_index << ") has two neighbors." << std::endl;
+        //     // if yes, loop again and test for uniqueness
+        //     int unique_nbr_flag = getFaceNeighbors(ii, ne_vertex_index, nw_vertex_index);
+
+        //     if (unique_nbr_flag == 1)
+        //     {
+        //         std::cout << "  This edge is also unique." << std::endl;
+
+        //         edge_list.push_back(edge_c);
+        //     }
+        //     else
+        //     {
+        //         continue;
+        //     }
+        // }
+        // else
+        // {
+        //     std::cout << "Edge with vertices (" << ne_vertex_index << ", " << nw_vertex_index << ") is on a boundary and hence unique." << std::endl;
+        //     edge_list.push_back(edge_c); 
+        // }
+
+        // edge_c.start_vertex = nw_vertex_index;
+        // edge_c.end_vertex = sw_vertex_index;
+
+
+        // neighbor_count = findFaceNeighbors(ii, nw_vertex_index, sw_vertex_index);
+        // if (neighbor_count > 0)
+        // {
+        //     std::cout << "Edge with vertices (" << nw_vertex_index << ", " << sw_vertex_index << ") has two neighbors." << std::endl;
+        //     // if yes, loop again and test for uniqueness
+        //     int unique_nbr_flag = getFaceNeighbors(ii, nw_vertex_index, sw_vertex_index);
+
+        //     if (unique_nbr_flag == 1)
+        //     {
+        //         std::cout << "  This edge is also unique." << std::endl;
+
+        //         edge_list.push_back(edge_c);
+        //     }
+        //     else
+        //     {
+        //         continue;
+        //     }
+        // }
+        // else
+        // {
+        //     std::cout << "Edge with vertices (" << nw_vertex_index << ", " << sw_vertex_index << ") is on a boundary." << std::endl;
+        //     edge_list.push_back(edge_c);
+
+        // }
 
     }
 }
